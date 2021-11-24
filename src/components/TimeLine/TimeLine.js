7145256b16e6ceb392241f06a1f541f63803673c
Socksham <60550018+Socksham@users.noreply.github.com>
2021-11-24 16:08:55 -0600

    simple change

--- src/components/TimeLine/TimeLine.js
+++ src/components/TimeLine/TimeLine.js
@@ -46,7 +46,7 @@ const Timeline = () => {
     <Section id="about">
       <SectionTitle>About Me</SectionTitle>
       <SectionText>
-      The purpose of JavaScript Mastery is to help aspiring and established developers to take their development skills to the next level and build awesome apps.
+      I am a hard-working and diligent student looking for research opportunities.
       </SectionText>
       <CarouselContainer ref={carouselRef} onScroll={handleScroll}>
         <>
